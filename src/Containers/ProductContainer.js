ba1d0528cb4ed94dd0687d8e64f1834733802b92
stomg7969 <stomg7969@gmail.com>
2019-04-28 16:01:27 -0400

    All products are now rendered even when not logged in

--- src/Containers/ProductContainer.js
+++ src/Containers/ProductContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import ProductCard from "../Components/ProductCard";
+import { STORE_PRODUCTS } from "../Types";
 
 class ProductContainer extends Component {
   componentDidMount() {
     const token = localStorage.getItem("user_token");
+    // token and authorization is unnecessary because users are able to see
+    // all products even when not logged in.
     fetch("http://localhost:3000/products", {
       method: "GET",
       headers: {
@@ -12,14 +15,14 @@ class ProductContainer extends Component {
       }
     })
       .then(r => r.json())
-      // Redux recommeded due to UserInputContainer.
+      // Redux recommeded due to UserInputContainer's props requirement.
       .then(products =>
-        this.props.dispatch({ type: "store_products", products: products })
+        this.props.dispatch({ type: STORE_PRODUCTS, products: products })
       );
   }
   render() {
     const productList = this.props.products.map(product => {
-      return <ProductCard product={product} />;
+      return <ProductCard key={product.id} product={product} />;
     });
     return (
       <div id="product-container">
