bcbca45554278aa6e947f0534065f44b6620be55
stomg7969 <stomg7969@gmail.com>
2019-05-21 21:55:23 -0400

    Deleted unnecessary comments

--- src/Components/DeleteAccount.js
+++ src/Components/DeleteAccount.js
@@ -8,23 +8,7 @@ const DeleteAccount = props => {
     const token = localStorage.getItem("user_token");
     const decoded = jwt.verify(token, process.env.REACT_APP_AUTH_KEY);
     console.log("%c Checking before delete", "color: green", decoded);
-    // Users can delete account, but their orders(dependents) will remain in the database
-    // because companies need to be able to keep track of their orders for accounting purpose.
-    // fetch(`${process.env.REACT_APP_HOST}/api/v1/users/${decoded.user_id}`, {
-    //   method: "DELETE",
-    //   headers: {
-    //     Authorization: `Bearer ${token}`
-    //   }
-    // }).then(r => {
-    //   if (!r.ok) {
-    //     console.log(r);
-    //     props.history.push("/user/profile");
-    //   } else {
-    //     localStorage.removeItem("user_token");
-    //     alert(`Deleted, bye bye ${decoded.username}`);
-    //     props.history.push("/");
-    //   }
-    // });
+    
     axios.delete(`${process.env.REACT_APP_HOST}/api/v1/users/${decoded.user_id}`, {
       headers: { Authorization: `Bearer ${token}` }
     })
